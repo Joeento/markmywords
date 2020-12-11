21f305b7808e8b5f32964655d6212f32eeb906dd
Eric Kudler <eric.kudler@hearst.com>
2020-12-11 15:06:32 -0500

    Initial commit

---
+++ src/main/java/com/kudler/markmywords/MarkmywordsApplication.java
@@ -0,0 +1,13 @@
+package com.kudler.markmywords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkmywordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkmywordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kudler/markmywords/MarkmywordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kudler.markmywords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkmywordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
