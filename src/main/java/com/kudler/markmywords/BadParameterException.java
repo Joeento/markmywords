b206c8c2d2a71c1a6f9c4890a3a4992d207b94c4
Eric Kudler <eric.kudler@hearst.com>
2020-12-12 16:22:07 -0500

    Creates runtime exceptions for user errors in my application

---
+++ src/main/java/com/kudler/markmywords/BadParameterException.java
@@ -0,0 +1,10 @@
+package com.kudler.markmywords;
+
+public class BadParameterException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public BadParameterException(String parameter, String type) {
+        super("Sorry, one of your parameters was invalid.  Please make sure you have a '" + parameter + "' field of type '" + type + "'.");
+    }
+}
---
+++ src/main/java/com/kudler/markmywords/FileUploadException.java
@@ -0,0 +1,10 @@
+package com.kudler.markmywords;
+
+public class FileUploadException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public FileUploadException(String msg) {
+        super(msg);
+    }
+}
