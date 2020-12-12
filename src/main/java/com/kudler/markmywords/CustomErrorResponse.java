835b9dc1dfb5f92f279f95790789421d27b26eba
Eric Kudler <eric.kudler@hearst.com>
2020-12-12 16:33:12 -0500

    Wrapper for our json error response

---
+++ src/main/java/com/kudler/markmywords/CustomErrorResponse.java
@@ -0,0 +1,30 @@
+package com.kudler.markmywords;
+
+import org.springframework.http.HttpStatus;
+
+public class CustomErrorResponse {
+
+    String errorMsg;
+    HttpStatus status;
+
+    public CustomErrorResponse(String errorMsg) {
+        super();
+        this.errorMsg = errorMsg;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+}
