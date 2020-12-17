41c19d5b72ba56f2ebe76cc7d9c288b26f13ed49
Eric Kudler <eric.kudler@hearst.com>
2020-12-17 01:15:17 -0500

    Documentation for exceptions

--- src/main/java/com/kudler/markmywords/exception/BadParameterException.java
+++ src/main/java/com/kudler/markmywords/exception/BadParameterException.java
@@ -1,5 +1,10 @@
 package com.kudler.markmywords.exception;
 
+/**
+ * Used to pass on capture and pass on information
+ * about API endpoint parameters that have been
+ * incorrectly formatted or missing.
+ */
 public class BadParameterException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
--- src/main/java/com/kudler/markmywords/exception/FileUploadException.java
+++ src/main/java/com/kudler/markmywords/exception/FileUploadException.java
@@ -1,5 +1,10 @@
 package com.kudler.markmywords.exception;
 
+/**
+ * Used to pass on capture and pass on information
+ * about errors that occur during file upload
+ * incorrectly formatted or missing.
+ */
 public class FileUploadException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
