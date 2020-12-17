21d75926a9cdbaa2684223cbf7890b43be8880f4
Eric Kudler <eric.kudler@hearst.com>
2020-12-17 01:19:41 -0500

    Added documentation for main class

--- src/main/java/com/kudler/markmywords/Application.java
+++ src/main/java/com/kudler/markmywords/Application.java
@@ -3,11 +3,18 @@ package com.kudler.markmywords;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class that activates the application
+ * and begins listening for requests sent to
+ * the endpoints in our controllers.
+ */
 @SpringBootApplication
 public class Application {
-
+	/**
+	 * Begin running server.
+	 * @param args arguments for webserver to start.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
